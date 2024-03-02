84440ba608dc4878c2fb10a7f97e893687f0d914
prateek sharma <ppprateeksharma006@gmail.com>
2024-03-02 15:53:22 +0530

    tower of hanoi  added into recursion_questions

--- recursion_questions.cpp
+++ recursion_questions.cpp
@@ -49,3 +49,27 @@ int main()
 }
 
 */
+
+
+/*
+// TOWER OF HANOI
+
+#include <iostream>
+
+using namespace std;
+
+void towerOfHanoi(int n, char src ,char des, char help)
+{   
+    if(n==0){ return; }
+
+    towerOfHanoi(n-1, src, help, des);
+    cout << "Move from " << src <<" to "<<des<<endl;
+    towerOfHanoi(n-1, help, des, src);
+}
+
+int main()
+{
+    towerOfHanoi(3, 'A', 'B', 'C');
+}
+
+*/
