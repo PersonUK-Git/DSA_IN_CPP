aee1ee8cd1ea1cc713a8f2bfef1e3658d6126287
prateek sharma <ppprateeksharma006@gmail.com>
2024-03-02 15:14:45 +0530

    replace PI to 3.14  added into recursion_questions

--- recursion_questions.cpp
+++ recursion_questions.cpp
@@ -21,4 +21,31 @@ int main()
 
 */
 
+// Replace pi with 3.14 in string
 
+/*
+#include <iostream>
+
+using namespace std;
+
+void replacePI(string s)
+{
+    if(s.length()==0){ return; }
+
+    if(s[0] == 'p' && s[1] == 'i')
+    {
+        cout << "3.14";
+        replacePI(s.substr(2));
+
+    }
+    else{
+        cout << s[0];
+        replacePI(s.substr(1));
+    }
+}
+int main()
+{
+    replacePI("pidsfrgsfpiippifsawe");
+}
+
+*/
