7fc90e0f1ba93f189d28acafbf7575e523a8bd04
prateek sharma <ppprateeksharma006@gmail.com>
2024-03-03 10:48:56 +0530

    Remove duplicate characters from a string is added to recursion file

--- recursion_questions.cpp
+++ recursion_questions.cpp
@@ -73,3 +73,31 @@ int main()
 }
 
 */
+
+/*
+//REMOVE DUPLICATE CHARACTERS FROM A STRING
+
+#include <iostream>
+
+using namespace std;
+
+string removeDupli(string s)
+{
+    if(s.length()==0){ return ""; }
+
+    char a = s[0];
+    string ans = removeDupli(s.substr(1));
+
+    if(a==ans[0]){
+        return ans;
+    }
+
+    return a+ans;
+}
+
+int main()
+{
+    cout<<removeDupli("rraannddoommtteesstt");
+}
+
+*/
